2439d591f0a5c50413c8e1de8676b51c02bde33d
Vilius <dragunas.vilius@gmail.com>
2023-10-31 21:10:58 +0900

    Prepare test case

--- build/build-packs.ts
+++ build/build-packs.ts
@@ -4,9 +4,25 @@ import url from "url";
 import { CompendiumPack, PackError } from "./lib/compendium-pack.ts";
 
 const asJson = process.argv[2]?.toLowerCase() === "json";
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-const packsDataPath = path.resolve(__dirname, "../packs");
-const packDirPaths = fs.readdirSync(packsDataPath).map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
+// const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+// const packsDataPath = path.resolve(__dirname, "../packs");
+// const packDirPaths = fs.readdirSync(packsDataPath).map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
+
+const packDirPaths = [ 
+    "C:\\Files\\repos\\pf2e-extractor\\packs\\pack",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs\\spell-effects",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/spells",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/conditions",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/equipment",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/ancestryfeatures",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/bestiary-ability-glossary-srd",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/action-macros",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/actions",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/feats",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/hazards",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/deities",
+    // "C:\\Files\\repos\\pf2e-extractor\\packs/pathfinder-bestiary-2",
+ ];
 
 // Loads all packs into memory for the sake of making all document name/id mappings available
 const packs = packDirPaths.map((p) => CompendiumPack.loadJSON(p));
