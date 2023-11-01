554c92da2ef071a93ba70ee49f9a12da2d4dad9b
Vilius <dragunas.vilius@gmail.com>
2023-11-01 20:03:09 +0900

    Prep pack for testing again

--- build/build-packs.ts
+++ build/build-packs.ts
@@ -2,35 +2,25 @@ import fs from "fs";
 import path from "path";
 import url from "url";
 import { CompendiumPack, PackError } from "./lib/compendium-pack.ts";
+import { forEach } from "remeda";
 
 const asJson = process.argv[2]?.toLowerCase() === "json";
-// const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-// const packsDataPath = path.resolve(__dirname, "../packs");
-// const packDirPaths = fs.readdirSync(packsDataPath).map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
-
-const packDirPaths = [ 
-    "C:\\Files\\repos\\pf2e-extractor\\packs\\pack",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs\\spell-effects",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/spells",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/conditions",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/equipment",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/ancestryfeatures",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/bestiary-ability-glossary-srd",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/action-macros",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/actions",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/feats",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/hazards",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/deities",
-    // "C:\\Files\\repos\\pf2e-extractor\\packs/pathfinder-bestiary-2",
- ];
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const packsDataPath = path.resolve(__dirname, "../packs");
+const packDirPaths = fs.readdirSync(packsDataPath).map((dirName) => path.resolve(__dirname, packsDataPath, dirName));
 
 // Loads all packs into memory for the sake of making all document name/id mappings available
 const packs = packDirPaths.map((p) => CompendiumPack.loadJSON(p));
-const documentCounts = await Promise.all(packs.map((p) => p.save(asJson)));
-const total = documentCounts.reduce((total, c) => total + c, 0);
+// const documentCounts = await Promise.all(packs.map((p) => p.save(asJson)));
+// const total = documentCounts.reduce((total, c) => total + c, 0);
 
-if (documentCounts.length > 0) {
-    console.log(`Created ${documentCounts.length} packs with ${total} documents.`);
-} else {
-    throw PackError("No data available to build packs.");
-}
+const pack = packs.filter((p) => p.packId == "test")[0];
+
+console.log(pack.data[0].items.filter((i) => i.name == "Champion Devotion Spells")[0]);
+console.log(pack.data[0].items.filter((i) => i.name == "Lay on Hands")[0]);
+
+// if (documentCounts.length > 0) {
+//     console.log(`Created ${documentCounts.length} packs with ${total} documents.`);
+// } else {
+//     throw PackError("No data available to build packs.");
+// }
